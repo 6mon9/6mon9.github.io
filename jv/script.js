6a3f741ee7d7a42c1264f6185e839966bd6ce8a0
Simon Levaufre <levaufresimon2004@gmail.com>
2021-04-29 19:18:05 +0200

    Premier commit

---
+++ jv/script.js
@@ -0,0 +1,17 @@
+
+function maFonction() {
+alert("Lors de la présentation du premier Iphone le 9 janvier 2007, ce dernier était TRES loin d'être finit et il comportait encore énormement de bug: Le Wifi coupait en permanence, le téléphone s'arretait si on écoutais de la musique trop longtemps, la batterie se vidait rapidement. pour parer a tout ça, Steve Jobs avait plusieur Iphone, c'est très discret mais ont peut voir qu'il ne prend jamais le même Iphone deux fois de suite.  ")
+}	
+function maFonction2() {
+alert("Voila c'est tout pour moi ! n'hésitez pas a cliqué sur un autre article pour découvrire le programme d'un de mes camarade ! ")
+}		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
